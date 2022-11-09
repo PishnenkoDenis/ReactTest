72c6db4b2b3a969e590b1af8c4ac16be161a5676
d.pishnenko <d.pishnenko@tg.dunice.net>
2022-11-09 18:05:49 +0300

    Add initial routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Header from './components/Header/Header';
+import LoginPage from './pages/LoginPage/LoginPage';
+import CountryListingPage from './pages/CountryListingPage/CountryListingPage';
+import CountryDetailsPage from './pages/CountryDetailsPage/CountryDetailsPage';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<CountryListingPage />} />
+          <Route path="/details/:id" element={<CountryDetailsPage />} />
+          <Route path="*" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
