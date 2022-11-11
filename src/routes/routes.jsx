7dc70b638f5c0c839fb847231b1b1108aff74f7d
d.pishnenko <d.pishnenko@tg.dunice.net>
2022-11-11 11:57:39 +0300

    Add routes

---
+++ src/routes/routes.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Navigate } from 'react-router';
+import CountryDetailsPage from '../pages/CountryDetailsPage/CountryDetailsPage';
+import CountryListingPage from '../pages/CountryListingPage/CountryListingPage';
+
+import LoginPage from '../pages/LoginPage/LoginPage';
+
+export const publicRoutes = [
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    path: '*',
+    element: <LoginPage />,
+  },
+];
+
+export const authRoutes = [
+  {
+    path: '/details/:id',
+    element: <CountryDetailsPage />,
+  },
+  {
+    path: '/index',
+    element: <CountryListingPage />,
+  },
+  {
+    path: '*',
+    element: <Navigate to="/index" />,
+  },
+];
